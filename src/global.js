29dc2fd8fcde2a89ecc4174ae511434e6c41602f
karthigayani <karthi1826@gmail.com>
2023-10-15 23:48:20 +0530

    connected to vercel

--- src/global.js
+++ src/global.js
@@ -1,3 +1,4 @@
 // const API = "https://6404dff940597b65de2c4412.mockapi.io";
 // export const API = "https://6404dff940597b65de2c4412.mockapi.io"; // exporting variable
-export const API = "http://localhost:4000"; // Connecting node with react app
+// export const API = "http://localhost:4000"; // Connecting node with react app
+export const API = "https://node-recap-delta.vercel.app"; // Connecting node deployment link with react app
