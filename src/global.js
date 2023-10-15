3bf584f225679442fc3bb173386b135bb91ab579
karthigayani <karthi1826@gmail.com>
2023-10-15 09:52:47 +0530

    replaced local node link with mockapi link

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
 // const API = "https://6404dff940597b65de2c4412.mockapi.io";
-export const API = "https://6404dff940597b65de2c4412.mockapi.io";
+// export const API = "https://6404dff940597b65de2c4412.mockapi.io"; // exporting variable
+export const API = "http://localhost:4000"; // Connecting node with react app
